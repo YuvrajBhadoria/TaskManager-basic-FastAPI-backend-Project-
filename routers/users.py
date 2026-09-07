add95c8b66024512b785614ee91366fb81230ac1
YuvrajBhadoria <byyuvraj406@gmail.com>
2026-09-07 19:52:14 +0530

    connecting JWT to /login

--- routers/users.py
+++ routers/users.py
@@ -10,11 +10,20 @@ def register(user:UserCreate):
     
 @router.post("/login")
 def login(user:UserCreate):
-    loggedIn = userService.login(user)
+    loggedIn:User = userService.login(user)
     if loggedIn is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="User Not Found"
         )
-    return loggedIn
+ 
+    SECRET_KEY = "my-secret-key"
+
+    payload = {
+        "sub": loggedIn.id
+    }
+
+    token = jwt.encode(payload , SECRET_KEY , algorithm="HS256")
+
+    return {"access_token": token}
     
